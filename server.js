981ac4effd10bc492dabd7736b824571e203fba7
KhengVatanautdom <156300148+KhengVatanautdom@users.noreply.github.com>
2025-01-29 23:22:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const http = require('http');
+
 http.createServer(function (req, res) {
-    res.write('I am Learning Cloud Developing');
-    res.end();
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, World!\n');
 }).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
