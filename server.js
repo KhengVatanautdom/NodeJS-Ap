4373686f42ffd82a638bbd66b82fae7c0bb57326
KhengVatanautdom <156300148+KhengVatanautdom@users.noreply.github.com>
2025-01-29 22:57:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello I am a cloud Developer');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from Node.js Server!');
+});
+
+server.listen(3000, () => {
+    console.log('Server running at http://YOUR_EC2_PUBLIC_IP:3000/');
+});
