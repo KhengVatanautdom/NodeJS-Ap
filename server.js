a85bcc2690c8d7cc4e08eb97611fefc2f1893ca8
KhengVatanautdom <156300148+KhengVatanautdom@users.noreply.github.com>
2025-01-30 12:52:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.write('I am Learning Cloud Developing');
   res.end();
-}).listen(3000, '0.0.0.0'); // Changed to port 3000
+}).listen(80, '0.0.0.0'); // Changed to port 3000
